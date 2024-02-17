9bba4b6b21d0a79655501b465038850e46ad968d
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2024-02-18 00:04:19 +0800

    Added different render modes for particles, glowing which adds blur, and gas which renders as transparent balls

--- src/js/sandfall.ts
+++ src/js/sandfall.ts
@@ -1,9 +1,25 @@
 class Renderer {
   canvas = document.querySelector("canvas")!;
   context = this.canvas.getContext("2d")!;
-  renderParticle(particle: Particle) {
-    this.context.fillStyle = particle.color;
-    this.context.fillRect(particle.x, particle.y, 1, 1);
+  renderParticle(particle: Particle, style="solid") {
+    if (style == "glowing"){
+      // Could be cool to render the color differently depending on temperature!
+      this.context.fillStyle = particle.color;
+      this.context.shadowColor = particle.color;
+      this.context.shadowBlur = 10;
+      this.context.fillRect(particle.x, particle.y, 1, 1);
+    }
+    if (style == "gas"){
+      this.context.beginPath();
+      // Need a function to convert the color
+      this.context.fillStyle = "rgba(255, 255, 255, 0.5)";
+      this.context.arc(particle.x, particle.y, 3, 0, 2 * Math.PI);
+      this.context.fill();
+    }
+    else {
+      this.context.fillStyle = particle.color;
+      this.context.fillRect(particle.x, particle.y, 1, 1);
+    }
   }
   clear() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -71,7 +87,7 @@ class Sandbox {
     renderer.clear();
     for (var particle of this.particles) {
       particle.newPos();
-      renderer.renderParticle(particle);
+      renderer.renderParticle(particle, "glowing");
       if (this.isOutOfBounds(particle, renderer)){
         this.clearParticle(particle);
       }
