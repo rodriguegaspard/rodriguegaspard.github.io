b294e50d52387992afb37522d3db82a9529e151a
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2024-02-17 18:44:16 +0800

    Tweaked the blog stylesheet with a better date format and borders

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,3 @@
 export function formatDate(date){
-  return new Date(date).toLocaleDateString("en-GB", {year:'numeric', month:'long', day:'numeric'});
+  return new Date(date).toLocaleDateString("en-GB");
 }
